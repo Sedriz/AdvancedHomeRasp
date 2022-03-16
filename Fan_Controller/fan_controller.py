f761d0ca78a5a801c4ccd44eac80d43dfd306e86
sedriz <sedrik.staack@gmx.de>
2022-03-16 23:32:03 +0100

    use gpiozero temp function

--- Fan_Controller/fan_controller.py
+++ Fan_Controller/fan_controller.py
@@ -3,18 +3,19 @@
 import sys
 import os
 import time
-import vcgencmd as vc
+from gpiozero import CPUTemperature
 from gpiozero import OutputDevice
 
 def main():
     fan = OutputDevice(18)
     while True:
-        temp = int(vc.measure_temp())
-        print(temp)
-        if  temp >= 50:
+        cpu = CPUTemperature()
+        temperature = cpu.temperature
+        print(temperature)
+        if  temperature >= 50:
             fan.on()
             print("fan.on()")
-        elif temp <= 45:
+        elif temperature <= 45:
             fan.off()
             print("fan.off()")
         time.sleep(1)
